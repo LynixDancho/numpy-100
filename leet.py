7dca4c6f39378228555e196f8eab3525c7a4a544
LynixDancho <96496354+LynixDancho@users.noreply.github.com>
2026-06-14 00:00:27 +0100

    ye

--- leet.py
+++ leet.py
@@ -1,43 +1,28 @@
-from typing import Optional
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+from typing import List
+
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-       newmerge=  ListNode()
-       tail = newmerge
-       while list1 and list2:
-           if list1.val< list2.val:
-               tail.next = list1
-               list1=list1.next
-               
-           else:
-               tail.next = list2
-               list2=list2.next
-           tail = tail.next
-               
-       if not list1:
-           tail.next = list2
-       else :
-           tail.next = list1 
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        max= float('inf')
+        index = None
+        for i in range(len(nums)):
+            if target == nums[i]:
+                return i 
+            if target > nums[i]:
+
+ 
+                    index = i +1
+        if index :
+            return index
+        else : return 0 
+        
 
 
-           
+            
 
+        
 
-       return newmerge.next
+ 
+yes = [1,3,5,6]
+test = Solution.searchInsert(None,yes , 2 )
 
-
-list1 = ListNode(1)
-list1.next = ListNode(2)
-list1.next.next = ListNode(4)
-
-
-# 1 -> 3 -> 4
-list2 = ListNode(1)
-list2.next = ListNode(3)
-list2.next.next = ListNode(4) 
-yes = Solution.mergeTwoLists(None,list1 = list1, list2 = list2)
-
-print(yes)
+print(test)
