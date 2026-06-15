06f48c2d0cc13387c7d50157facd0a44a388a99f
LynixDancho <96496354+LynixDancho@users.noreply.github.com>
2026-06-16 00:40:13 +0100

    o

--- leet.py
+++ leet.py
@@ -1,27 +1,8 @@
 from typing import List
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        digits = digits[::-1]
-        if digits[0] != 9:
-            digits[0]=digits[0]+1
-            return digits[::-1]
-        for i in range(len(digits)):
-            
-            if digits[i] !=9:    
-                print(digits[i])
-                digits[i] = digits[i] +1
-                return digits[::-1]
-            digits[i] =0
-            
-        digits.append(1)
-        return digits[::-1]
-
-
-
-
-
-
-yes = Solution.plusOne(None,[8,9,9,9])
+    def mySqrt(self, x: int) -> int:
+        x.
+yes = Solution
 
 
 
